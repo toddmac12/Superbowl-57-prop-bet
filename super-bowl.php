792d08fbaf8a75e3f38355af159ea95b0192cd0c
toddmac12 <toddmac12@users.noreply.github.com>
2023-02-12 17:47:21 -0500

    Add files via upload

---
+++ super-bowl.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Super Bowl Prop Bets Score Sheet</title>
+</head>
+<body>
+<h1>Super Bowl Prop Bets Score Sheet</h1>
+
+<form>
+	<label for="coin-toss">1. What will be the result of the opening coin toss?</label>
+	<select id="coin-toss">
+		<option value="heads">Heads</option>
+		<option value="tails">Tails</option>
+	</select>
+
+	<br>
+
+	<label for="first-quarter">2. Which team will be leading at the end of the first quarter?</label>
+	<select id="first-quarter">
+		<option value="eagles">Eagles</option>
+		<option value="chiefs">Chiefs</option>
+	</select>
+
+	<br>
+
+	<label for="first-score">3. Which team will score five points or more first?</label>
+	<select id="first-score">
+		<option value="eagles">Eagles</option>
+		<option value="chiefs">Chiefs</option>
+	</select>
+
+	<br>
+
+	<label for="mahomes-touchdowns">4. Will Patrick Mahomes have over or under 1.5 passing touchdowns?</label>
+	<select id="mahomes-touchdowns">
+		<option value="over">Over</option>
+		<option value="under">Under</option>
+	</select>
+
+	<br>
+
+	<label for="hurts-passing">5. Jalen Hurts over/under 243.5 passing yards?</label>
+	<select id="hurts-passing">
+		<option value="over">Over</option>
+		<option value="under">Under</option>
+	</select>
+
+	<br>
+
+	<label for="total-touchdowns">6. Total touchdowns scored: over/under 6.5</label>
+	<select id="total-touchdowns">
+		<option value="over">Over</option>
+		<option value="under">Under</option>
+	</select>
+
+	<br>
+
+	<label for="highest-scoring-half">7. Highest scoring half?</label>
+	<select id="highest-scoring-half">
+		<option value="first">First</option>
+		<option value="second">Second</option>
+	</select>
+
+	<br>
+
+	<label for="super-bowl-mvp">8. Will a quarterback win Super Bowl MVP?</label>
+	<select id="super-bowl-mvp">
+		<option value="yes">Yes</option>
+		<option value="no">No</option>
+	</select>
+
+	<br>
+
+	<label for="last-points">9. Which team will score the last points?</label>
+	<select id="last-points">
+		<option value="eagles">Eagles</option>
+		<option value="chiefs">Chiefs</option>
+	</select>
+
+	<br>
+
+	<label for="eagles-every-quarter">10. Will the Philadelphia Eagles score in every quarter?</label>
+	<select id="eagles-every-quarter">
+		<option value="yes">Yes</option>
+		<option value="no">No</option>
+	</select>
+
+	<br>
+
+	<label for="total-points">11. Total points odd or even?</label>
+	<select id="total-points">
+		<option value="odd">Odd</option>
+		<option value="even">Even</option>
+	</select>
+
+	<br>
+<label for="score-firstquarter">12. BOTH TEAMS TO SCORE IN THE FIRST QUARTER?
+</label>
+	<select id="score-firstquarter">
+		<option value="Yes">Yes</option>
+		<option value="No">No</option>
+	</select>
+
+	<br>
+    <label for="travis-kelce">13. WILL TRAVIS KELCE SCORE A TOUCHDOWN?
+</label>
+	<select id="travis-kelce">
+		<option value="Yes">Yes</option>
+		<option value="No">No</option>
+	</select>
+
+	<br>
+    <label for="first-field-goal">14. WHICH TEAM WILL HAVE THE FIRST MADE FIELD GOAL?
+</label>
+	<select id="first-field-goal">
+		<option value="Eagles">Eagles</option>
+		<option value="even">Chiefs</option>
+	</select>
+
+	<br>
+    <label for="score-first">15. WHICH TEAM WILL SCORE FIRST?
+</label>
+	<select id="score-first">
+		<option value="Eagles">Eagles</option>
+		<option value="even">Chiefs</option>
+	</select>
+
+	<br>
+    <label for="aj-brown">16. AJ BROWN OVER/UNDER 72.5 RECEIVING VAROS?
+</label>
+	<select id="aj-brown">
+		<option value="over">Over</option>
+		<option value="under">Under</option>
+	</select>
+
+	<br>
+    <label for="patrick-mahomes">17. PATRICK MAHOMES OVER/UNDER 290.5 PASSING YARDS?
+</label>
+	<select id="patrick-mahomes">
+		<option value="over">Over</option>
+		<option value="under">Under</option>
+	</select>
+
+	<br>
+    <label for="both-teams-25">18. WILL BOTH TEAMS SCORE 25 OR MORE POINTS?
+</label>
+	<select id="both-teams-25">
+		<option value="Yes">Yes</option>
+		<option value="No">No</option>
+	</select>
+
+	<br>
+    <label for="both-teams-score-3rdQ">19. BOTH TEAMS TO SCORE IN THE THIRD QUARTER?
+</label>
+	<select id="both-teams-score-3rdQ">
+		<option value="Yes">Yes</option>
+		<option value="No">No</option>
+	</select>
+
+	<br>
+    <label for="miles-sanders">20. WILL MILES SANDERS SCORE A TOUCHDOWN?
+</label>
+	<select id="miles-sanders">
+		<option value="Yes">Yes</option>
+		<option value="No">No</option>
+	</select>
+
+	<br>
+    <label for="devonta-smith">21. DEVONTA SMITH OVER/UNDER 64.5 RECEIVING YARDS?
+</label>
+	<select id="devonta-smith">
+		<option value="over">Over</option>
+		<option value="under">Under</option>
+	</select>
+
+	<br>
+    <label for="jerrick-mckinnon">22. JERRICK MCKINNON OVER/UNDER 22.5 RECEIVING VAROS
+</label>
+	<select id="jerrick-mckinnon">
+		<option value="over">Over</option>
+		<option value="under">Under</option>
+
+	<br>
+    <label for="isiah-pacheco">23. ISIAH PACHECO OVER/UNDER 48.5 RUSHING VAROS?
+</label>
+	<select id="isiah-pacheco">
+		<option value="over">Over</option>
+		<option value="under">Under</option>
+	</select>
+
+	<br>
+    <label for="any-player-2+">24. WILL ANY PLAYER SCORE2+ TOUCHDOWNS? (QUARTERBACK PASSING ms DON'T COUNT)
+</label>
+	<select id="any-player-2+">
+		<option value="Yes">Yes</option>
+		<option value="No">No</option>
+	</select>
+
+	<br>
+    <label for="coach-gatorade-water">25. WILL THE WINNING COACH HAVE GATORADE OR WATER POURED ON HIM?
+</label>
+	<select id="coach-gatorade-water">
+		<option value="Gatorade">Gatorade</option>
+		<option value="Water">Water</option>
+	</select>
+
+	<br>
+	
+
+
+</body>
+</html>
